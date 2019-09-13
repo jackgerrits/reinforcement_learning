efc3680c2eb05f6c1b115b4ab31c297827c6aecc
Jack Gerrits <jackgerrits95@gmail.com>
2019-09-13 10:21:14 -0400

    WIP 2

--- include/event_builder.h
+++ include/event_builder.h
@@ -1,252 +1,330 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <numeric>
 
 #include "live_model.h"
 #include "ranking_response.h"
+#include "decision_response.h"
 
-struct delete_default_constructor
-{
-  delete_default_constructor() = delete;
-};
 
 struct cb_problem_type
 {
   using response_t = reinforcement_learning::ranking_response;
 };
 
-struct json_representation
+// struct ccb_problem_type
+// {
+//   using response_t = reinforcement_learning::decision_response;
+// };
+
+struct slate_problem_type
 {
-  using emit_type = std::string;
-
-  struct feature
-  {
-    feature(std::string name, float value) : str(name), value(value) {}
-    feature(std::string name, std::string value) : str(name + value) {}
-    feature(std::string name) : str(name) {}
-
-    std::string emit() const
-    {
-      std::stringstream ss;
-      ss << R"(")" << str << R"(":)" << value;
-      return ss.str();
-    }
-
-    std::string str;
-    float value = 1.0f;
-  };
-
-  struct continuous_action
-  {
-    continuous_action(std::string ref) : ref(ref) {}
-    std::string ref;
-  };
-
-  struct feat_namespace
-  {
-    feat_namespace(std::vector<feature> features) : features(features) {}
-    feat_namespace(std::string name, std::vector<feature> features) : str(name), features(features) {}
-
-    std::string emit() const
-    {
-      std::stringstream ss;
-      ss << R"(")" << str << R"(":{)";
-      for (auto &feature : features)
-      {
-        ss << feature.emit() << R"(,)";
-      }
-      ss.seekp(-1, std::ios_base::end);
-      ss << R"(})";
-      return ss.str();
-    }
-
-    std::string str = "";
-    std::vector<feature> features;
-  };
-
-  struct feature_space
-  {
-    feature_space(std::vector<feat_namespace> namespaces) : namespaces(namespaces) {}
-
-    std::string emit() const
-    {
-      std::stringstream ss;
-      for (auto &ns : namespaces)
-      {
-        ss << ns.emit() << ",";
-      }
-      auto ret = ss.str();
-      ret.pop_back();
-      return ret;
-    }
-
-    std::vector<feat_namespace> namespaces;
-  };
-
-  struct slot
-  {
-    slot(feature_space context, std::vector<int> actions) : namespaces(namespaces) {}
-
-    std::string emit() const
-    {
-      // std::stringstream ss;
-      // for (auto &ns : namespaces)
-      // {
-      //   ss << ns.emit() << ",";
-      // }
-      // auto ret = ss.str();
-      // ret.pop_back();
-      // return ret;
-    }
-
-    std::vector<feat_namespace> namespaces;
-  };
-
-  template <typename TProblemType>
-  struct problem_data
-  {
-  };
-
-  template <typename TProblemType>
-  static std::string emit(std::string id, feature_space shared_context, problem_data<TProblemType>);
+  using response_t = reinforcement_learning::decision_response;
 };
 
-template <>
-struct json_representation::problem_data<cb_problem_type>
+struct feature
 {
-  problem_data(std::vector<feature_space> actions) : actions(actions){};
+  feature() {}
+  feature(std::string const& name, float value) : name(name), value(value) {}
+  feature(std::string const& name, std::string const& value) : name(name + value) {}
+  feature(std::string const& name) : name(name) {}
+  std::string name;
+  float value = 1.0f;
+};
 
-  std::string emit() const
+struct continuous_action
+{
+  continuous_action(std::string const& ref) : ref(ref) {}
+  std::string ref;
+};
+
+struct feat_namespace
+{
+  feat_namespace() {}
+  feat_namespace(std::vector<feature> const& features) : features(features) {}
+  feat_namespace(std::string const& name, std::vector<feature> const& features) : name(name), features(features) {}
+  std::string name = "";
+  std::vector<feature> features;
+};
+
+struct feature_space
+{
+  feature_space() = default;
+  feature_space(std::vector<feat_namespace> const& namespaces) : namespaces(namespaces) {}
+
+  void push_namespace(feat_namespace const& ns)
   {
-    std::stringstream ss;
-    ss << R"("_multi":[)";
-    for (auto &action : actions)
+    namespaces.push_back(ns);
+  }
+  void push_namespace(feat_namespace const&& ns)
+  {
+    namespaces.emplace_back(ns);
+  }
+
+  std::vector<feat_namespace> namespaces;
+};
+
+struct slot
+{
+  slot() = default;
+  slot(feature_space const& context, std::vector<uint32_t> const& actions, std::string const& id) : slot_context(context), _explicit_included_actions(actions), _id(id) {}
+  slot(feature_space const& context, std::vector<uint32_t> const& actions) : slot_context(context), _explicit_included_actions(actions) {}
+  slot(std::vector<uint32_t> const& actions, std::string const& id) : _explicit_included_actions(actions), _id(id) {}
+  slot(feature_space const& context) : slot_context(context) {}
+  slot(feature_space const& context, std::string const& id) : slot_context(context), _id(id) {}
+  slot(std::vector<uint32_t> const& actions) : _explicit_included_actions(actions) {}
+  slot(std::string const& id) : _id(id) {}
+
+  feature_space slot_context;
+  std::vector<uint32_t> _explicit_included_actions;
+  std::string _id = "";
+};
+
+
+struct builder
+{
+  builder() = default;
+
+  template <typename TSerializer>
+  typename TSerializer::emit_type emit()
+  {
+    return serializer.serialize(shared_context, actions, slots);
+  }
+
+  feature_space shared_context;
+  std::vector<feature_space> actions;
+  std::vector<slot> slots;
+};
+
+struct json_serializer
+{
+  using serialized_t = std::string;
+
+  static serialized_t serialize(feature_space const& shared, std::vector<feature_space> const& actions, std::vector<slot> const& slots)
+  {
+    std::ostringstream ss;
+    ss << "{";
+    serialize(ss, shared);
+    ss << R"(,"_multi":[)";
+    for (auto& action : actions)
     {
-      ss << "{" << action.emit() << "},";
+      ss << "{";
+      serialize(ss, action);
+      ss << "},";
     }
     ss.seekp(-1, std::ios_base::end);
     ss << R"(])";
 
+    if (slots.size() > 0)
+    {
+      ss << R"(,"_slots":[)";
+      for (auto& slot : slots)
+      {
+        ss << "{";
+        serialize(ss, slot);
+        ss << "},";
+      }
+      ss.seekp(-1, std::ios_base::end);
+      ss << R"(])";
+    }
+
+    ss << "}";
     return ss.str();
   }
 
+  static serialized_t serialize(builder const& b)
+  {
+    return serialize(b.shared_context, b.actions, b.slots);
+  }
+
+  static void serialize(std::ostringstream& ss, slot const& s)
+  {
+
+    auto i = ss.tellp();
+    serialize(ss, s.slot_context);
+    auto i2 = ss.tellp();
+    
+
+    if (s._explicit_included_actions.size() > 0)
+    {
+      if (i2 > i)
+      {
+        ss << ",";
+      }
+
+      ss << R"("_a":[)";
+      bool first_run = true;
+
+      for (auto& x : s._explicit_included_actions)
+      {
+        if (first_run)
+        {
+          first_run = false;
+        }
+        else
+        {
+          ss << ",";
+        }
+        ss << x;
+
+      }
+      ss << "]";
+    }
+
+    if (s._id != "")
+    {
+      if (s._explicit_included_actions.size() > 0)
+      {
+        ss << ",";
+      }
+      ss << R"("_id":")" << s._id << "\"";
+    }
+  }
+
+  static void serialize(std::ostringstream& ss, feature_space const& fs)
+  {
+    bool first_run = true;
+    if (fs.namespaces.size() > 0)
+    {
+      for (auto& ns : fs.namespaces)
+      {
+        if (first_run)
+        {
+          first_run = false;
+        }
+        else
+        {
+          ss << ",";
+        }
+        serialize(ss, ns);
+      }
+    }
+  }
+
+  static void serialize(std::ostringstream& ss, feat_namespace const& fn)
+  {
+    if (fn.name != "")
+    {
+      ss << "\"" << fn.name << R"(":{)";
+    }
+    bool first_run = true;
+    for (auto& feature : fn.features)
+    {
+      if (first_run)
+      {
+        first_run = false;
+      }
+      else
+      {
+        ss << ",";
+      }
+      serialize(ss, feature);
+    }
+    if (fn.name != "")
+    {
+      ss << "}";
+    }
+  }
+
+  static void serialize(std::ostringstream& ss, feature const& f)
+  {
+    ss << R"(")" << f.name << R"(":)" << f.value;
+  }
+};
+
+template <typename TProblemType>
+struct problem_data
+{
+  // Specializations MUST be used.
+  problem_data() = delete;
+};
+
+struct slate_slot
+{
+  // ActionSets for Slates do not overlap, so the provided interface only allows for that.
+  slate_slot(feature_space const& shared_context, std::vector<feature_space> actions) : shared_context(shared_context), actions(actions) {}
+
+  feature_space shared_context;
   std::vector<feature_space> actions;
 };
 
 template <>
-std::string json_representation::emit<cb_problem_type>(std::string id, feature_space shared_context, problem_data<cb_problem_type>);
-
-struct vw_representation
+struct problem_data<cb_problem_type>
 {
+  problem_data(std::vector<feature_space> const& actions) : actions(actions) {}
+  std::vector<feature_space> actions;
+  void add_to(builder& b) const
+  {
+    for (auto& action : actions)
+    {
+      b.actions.push_back(action);
+    }
+  }
 };
 
-struct delete_default_constructor
+template <>
+struct problem_data<slate_problem_type>
 {
-  delete_default_constructor() = delete;
+  problem_data(std::vector<slate_slot> const& slots) : slots(slots) {}
+
+  std::vector<slate_slot> slots;
+  void add_to(builder& b) const
+  {
+    for (auto& slot : slots)
+    {
+      uint32_t current_last_index_plus_one = static_cast<uint32_t>(b.actions.size());
+      for (auto& action : slot.actions)
+      {
+        b.actions.push_back(action);
+      }
+
+      std::vector<uint32_t> indicies(slot.actions.size());
+      std::iota(indicies.begin(), indicies.end(), current_last_index_plus_one);
+
+      b.slots.push_back({ slot.shared_context, indicies });
+    }
+  }
 };
 
-template <typename TProblemType, typename TRepresentation>
+template <typename TProblemType>
 struct problem_event
 {
-  problem_event();
-  problem_event(std::string id);
+  problem_event() {}
+  problem_event(std::string const& id) : id(id) {}
 
-  void shared_context(typename TRepresentation::feature_space &shared_context)
+  void shared_context(feature_space const& shared_context)
   {
-    m_shared_context = shared_context;
+    b.shared_context = shared_context;
   }
 
-  void event_problem(typename TRepresentation::template problem_data<TProblemType> &problem_data)
+  void event_problem(problem_data<TProblemType> const& problem_data)
   {
-    m_problem_data = problem_data;
+    problem_data.add_to(b);
   }
 
-  typename TRepresentation::emit_type emit() const
+  template <typename TRepresentation>
+  typename TRepresentation::serialized_t emit()
   {
-    return TRepresentation::emit(id, m_shared_context, m_problem_data);
+    return TRepresentation::serialize(b);
   }
 
   std::string id;
-  typename TRepresentation::feature_space m_shared_context;
-  typename TRepresentation::template problem_data<TProblemType> m_problem_data;
+  builder b;
 };
 
-
-
-// class logger
-// {
-//   template <typename TProblemType>
-//   void log_response(TProblemType::response resp);
-
-//   template <typename TProblemType>
-//   void log_reward(TProblemType::response resp, float reward);
-//   template <typename TProblemType>
-//   void log_reward(std::string event_id, float reward);
-// };
-
-//class SlateProblemType
-//{
-//  template <typename TRepresentation>
-//  class instance {
-//    instance(std::vector<SlateSlot<TRepresentation>> slots);
-//  };
-//
-//  class response {
-//  };
-//
-//  template <typename TRepresentation>
-//  class SlateSlot {
-//    SlateSlot(feature_space shared_context, std::vector<feature_space>);
-//    SlateSlot(feature_space shared_context, continuous_action);
-//  };
-//
-//  template<>
-//  class instance<json_representation> {
-//    instance(std::vector<SlateSlot<TRepresentation>> slots);
-//  };
-//
-//  template <>
-//  class SlateSlot<json_representation> {
-//    SlateSlot(feature_space shared_context, std::vector<feature_space>);
-//    SlateSlot(feature_space shared_context, continuous_action);
-//  };
-//};
-
-//class problem_event<SlateProblemType, json_representation>
-//{
-//  problem_event();
-//  problem_event(std::string id);
-//
-//  void shared_context(feature_space shared_context);
-//  void event_problem(SlateProblemType::Instance<json_representation>);
-//  std::string emit();
-//
-//  std::string;
-//  feature_space shared_context;
-//};
-
-template <typename TRepresentation>
-struct solver : delete_default_constructor
+struct estimator
 {
-};
-
-template <>
-struct solver<json_representation>
-{
-  solver();
-  solver(reinforcement_learning::live_model *lm);
+  estimator();
+  estimator(reinforcement_learning::live_model* lm);
 
   template <typename TProblemType, typename... Ts>
-  typename TProblemType::response_t predict_and_log(problem_event<TProblemType, json_representation> &instance, Ts &&...) = delete;
+  typename TProblemType::response_t predict_and_log(problem_event<TProblemType> const& instance, Ts&& ...) = delete;
 
-  reinforcement_learning::live_model *lm;
+  reinforcement_learning::live_model* lm;
 };
 
 template <>
-inline cb_problem_type::response_t solver<json_representation>::predict_and_log<cb_problem_type>(problem_event<cb_problem_type, json_representation> &instance)
+inline slate_problem_type::response_t estimator::predict_and_log<slate_problem_type>(problem_event<slate_problem_type> const& instance)
 {
-  auto str = instance.emit();
-  cb_problem_type::response_t resp;
-  lm->choose_rank(instance.id.c_str(), str.c_str(), resp);
-  return resp;
 }
--- unit_test/event_builder_test.cc
+++ unit_test/event_builder_test.cc
@@ -7,14 +7,92 @@
 
 #include "event_builder.h"
 
-BOOST_AUTO_TEST_CASE(json_cb_problem_type)
-{
-  cb_problem_type::instance<json_representation> prob({{{{"a", {{"a", 0.5}}}}}});
-  auto str = prob.emit();
-  BOOST_CHECK_EQUAL(str, R"("_multi":[{"a":{"a":0.5}}])");
 
-  cb_problem_type::instance<json_representation> prob2({{{{"a", {{"a", 0.5}}}, {"b", {{"a"}}}}},
-                                                        {{{"other", {{"new", 0.5}}}, {"b", {{"a", "b"}}}}}});
-  auto str2 = prob2.emit();
-  BOOST_CHECK_EQUAL(str2, R"("_multi":[{"a":{"a":0.5},"b":{"a":1}},{"other":{"new":0.5},"b":{"ab":1}}])");
+BOOST_AUTO_TEST_CASE(json_serialize_cb_problem)
+{
+  problem_data<cb_problem_type> prob({ feature_space{{feat_namespace{"a", {feature{"a", 0.5}}}}} });
+  feature_space shared(std::vector<feat_namespace>{ { {"one", { {"test"} }}}});
+  problem_event<cb_problem_type> event("eventid");
+  event.shared_context(shared);
+  event.event_problem(prob);
+
+  auto result = event.emit<json_serializer>();
+  BOOST_CHECK_EQUAL(result, R"({"one":{"test":1},"_multi":[{"a":{"a":0.5}}]})");
+}
+
+
+BOOST_AUTO_TEST_CASE(json_serialize_slate_problem)
+{
+  problem_data<slate_problem_type> prob({
+    {
+      feature_space(),
+      {
+        feature_space{{feat_namespace{"a", {feature{"a", 0.5}}}}}
+      }
+    }
+    });
+  feature_space shared(std::vector<feat_namespace>{ { {"one", { {"test"} }}}});
+  problem_event<slate_problem_type> event("eventid");
+  event.shared_context(shared);
+  event.event_problem(prob);
+
+  auto result = event.emit<json_serializer>();
+  BOOST_CHECK_EQUAL(result, R"({"one":{"test":1},"_multi":[{"a":{"a":0.5}}],"_slots":[{"_a":[0]}]})");
+}
+
+template<typename T>
+std::string to_string(T const& item)
+{
+  std::ostringstream ss;
+  json_serializer::serialize(ss, item);
+  return ss.str();
+}
+
+BOOST_AUTO_TEST_CASE(json_serialize_feature)
+{
+  BOOST_CHECK_EQUAL(to_string(feature("f", 1.0)), R"("f":1)");
+  BOOST_CHECK_EQUAL(to_string(feature("f", 1.5)), R"("f":1.5)");
+  BOOST_CHECK_EQUAL(to_string(feature("f")), R"("f":1)");
+}
+
+BOOST_AUTO_TEST_CASE(json_serialize_namespace)
+{
+  BOOST_CHECK_EQUAL(to_string(feat_namespace("test", { {"f", 1.0} })), R"("test":{"f":1})");
+  BOOST_CHECK_EQUAL(to_string(feat_namespace({ {"f", 1.0}, {"g", 4.5} })), R"("f":1,"g":4.5)");
+}
+
+BOOST_AUTO_TEST_CASE(json_serialize_feature_space)
+{
+  BOOST_CHECK_EQUAL(to_string(feature_space({ { "test", { {"f", 1.0} } } })), R"("test":{"f":1})");
+  BOOST_CHECK_EQUAL(to_string(feature_space({ { "test", { {"f", 1.0} } }, { "test", { {"f", 1.0} } } })), R"("test":{"f":1},"test":{"f":1})");
+  BOOST_CHECK_EQUAL(to_string(feature_space({ { "test", { {"f", 1.0} } }, { { {"f", 1.0} } },{ { {"g", 5.5} } } })), R"("test":{"f":1},"f":1,"g":5.5)");
+  BOOST_CHECK_EQUAL(to_string(feature_space({ { "test", { {"f", 1.0} } }, { { {"f", 1.0} } },{ { {"g", 5.5} } }, { "another", { {"feat", -1.0} } } })), R"("test":{"f":1},"f":1,"g":5.5,"another":{"feat":-1})");
+}
+
+BOOST_AUTO_TEST_CASE(json_serialize_slot)
+{
+  BOOST_CHECK_EQUAL(to_string(slot()), "");
+  BOOST_CHECK_EQUAL(to_string(slot("event_id")), R"("_id":"event_id")");
+  BOOST_CHECK_EQUAL(to_string(slot({ 1,2,3 }, "event_id")), R"("_a":[1,2,3],"_id":"event_id")");
+  BOOST_CHECK_EQUAL(to_string(slot(feature_space({ { "test", { {"f", 1.0} } } }))), R"("test":{"f":1})");
+  BOOST_CHECK_EQUAL(to_string(slot(feature_space({ { "test", { {"f", 1.0} } } }), std::vector<uint32_t>{ 1, 2, 3 })), R"("test":{"f":1},"_a":[1,2,3])");
+  BOOST_CHECK_EQUAL(to_string(slot(feature_space({ { "test", { {"f", 1.0} } } }), { 1,2,3 }, "event_id")), R"("test":{"f":1},"_a":[1,2,3],"_id":"event_id")");
+  BOOST_CHECK_EQUAL(to_string(slot(feature_space({ {{ {"f", 1.0}, {"g_feat", -5.3} } } }), { 1,2,3 }, "event_id")), R"("f":1,"g_feat":-5.3,"_a":[1,2,3],"_id":"event_id")");
+}
+
+BOOST_AUTO_TEST_CASE(json_serialize_builder)
+{
+  builder b;
+  b.actions = { 
+    feature_space{{
+      { "test", { {"f", 1.0} } },
+      { { {"f", 1.0} } },{ { {"g", 5.5} } },
+      { "another", { {"feat", -1.0} } } }
+    },
+    feature_space{{
+      { "test", { {"f", 1.0} } },
+      { { {"f", 1.0} } },{ { {"g", 5.5} } },
+      { "another", { {"feat", -1.0} } } }
+    },
+  };
 }
